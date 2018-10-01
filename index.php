f23149ce9439096f572ef6ac79bfeecbe3b58da3
akamata <akamata@JtrustcNP19.local>
2018-10-01 21:38:27 +0900

    ReplyToken_test

--- index.php
+++ index.php
@@ -1,10 +1,24 @@
 <?php
 
-//Composerでインストールしたライブラリを一括読み込み
-require_once __DIR__ . '/vendor/autoload.php';
+// Composerでインストールしたライブラリを一括読み込み
+require_once __DIR__ . 'vendor/autoload.php';
 
-//POSTメソッドで渡される値を取得、表示
-$inputString = file_get_contents('php://input');
-error_log($inputString);
+// アクセストークンを使いCurlHTTPClientをインスタンス化
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
+
+// CurlHTTPClientとシークレットを使いLINEBotをインスタンス化
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
+
+// LINE Message APIがリクエストに付与した署名を取得
+$signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+
+//  署名が正当かチェック。正当であればリクエストをパースし配列へ
+$events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
+
+// 配列に格納された書くイベントをループで処理
+foreach ($events as $event){
+  // テキストを返信
+  $bot->replyText($event->getReplyToken(), 'TextMessage');
+}
 
 ?>
