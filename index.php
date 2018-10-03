541542e891f4af042f44a7bd06b3a758363b0c33
akamata <akamata@JtrustcNP19.local>
2018-10-03 15:37:57 +0900

    reply-test1

--- index.php
+++ index.php
@@ -222,7 +222,7 @@ foreach ($events as $event) {
     if ($start == 'スタート') {
       //include('test.php');
       $bot->replyText($event->getReplyToken(), 'シミュレーションを開始します', '勤務先の都道府県を入力してください');
-      @prefecture = getText();
+      $prefecture = getText();
       replyTextMessage($bot, $replyToken, $prefecture);
       // オウム返し。
         //$bot->replyText($event->getReplyToken(), $event->getText());
