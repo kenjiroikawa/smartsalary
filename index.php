a45489d70e377e0df8f8a3b3e315bb405f318045
akamata <akamata@JtrustcNP19.local>
2018-10-21 18:59:31 +0900

    bonus-cal-test-v1.02

--- index.php
+++ index.php
@@ -1549,7 +1549,7 @@ if($dependants == 0 ){
 
   $message3 = "【スマートサラリー導入後】\n\n会社負担家賃（家賃×0.8）：$calculation[16]円\n本人負担家賃（家賃×0.2）：$calculation[17]円\n\n月額給与：$calculation[18]円\n年間賞与：$calculation[9]円\n年収：$calculation[19] 円\n\n健康保険料：$calculation[20]円\n厚生年金保険料：$calculation[21]円\n所得税：$calculation[22]円\n住民税：$calculation[23]円\n社保、税金、家賃控除後の可処分所得：$calculation[24]円\n\nスマートサラリー導入効果：$calculation[25]円\n";
 
-  $message4 = "【開発確認用パラメータ】\n\n年収：$calculation[10]円\n給与所得控除：$calculation[26]円\n所得控除：$calculation[27]円\n住民税年額：$calculation[28]円\n住民税月額：$calculation[29]円\n\n所得税差分：$calculation[30]円\n社会保険料差分：$calculation[31]円\n住民税差分：$calculation[32]円\n可処分所得増加分の検算：$calculation[33]円\n\n賞与の社会保険料$calculation[34]円\n賞与の社会保険料控除後の金額$calculation[35]円\n 賞与の源泉徴収金額$calculation[36]円"";
+  $message4 = "【開発確認用パラメータ】\n\n年収：$calculation[10]円\n給与所得控除：$calculation[26]円\n所得控除：$calculation[27]円\n住民税年額：$calculation[28]円\n住民税月額：$calculation[29]円\n\n所得税差分：$calculation[30]円\n社会保険料差分：$calculation[31]円\n住民税差分：$calculation[32]円\n可処分所得増加分の検算：$calculation[33]円\n\n賞与の社会保険料$calculation[34]円\n賞与の社会保険料控除後の金額$calculation[35]円\n 賞与の源泉徴収金額$calculation[36]円";
 
   // メッセージをユーザーに返信
 $bot->replyText($event->getReplyToken(), $message0, $message1, $message2, $message3, $message4);
